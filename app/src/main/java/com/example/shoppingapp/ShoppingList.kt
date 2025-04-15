530f806723ccd50c61e23939ca1e9483cdc39d5a
Shubham <shubhamsinghmys@gmail.com>
2025-04-15 14:00:24 +0530

    LazyColumn

--- app/src/main/java/com/example/shoppingapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppingapp/ShoppingList.kt
@@ -1,9 +1,5 @@
 package com.example.shoppingapp
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,17 +17,37 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.shoppingapp.ui.theme.ShoppingAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ShoppingAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ShoppingApp(paddingValues = innerPadding)
-                }
+data class ShoppingItem(val id: Int,
+                        var name: String,
+                        var quality: Int,
+                        var isEditing: Boolean = false)
+
+
+
+
+@Composable
+fun ShoppingApp(paddingValues: PaddingValues) {
+    var shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+
+    Column(
+        modifier = Modifier.padding(paddingValues)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")
+        }
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(count = 0) {
+
             }
         }
     }
