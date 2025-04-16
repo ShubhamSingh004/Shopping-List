01534c6f0a56ceb90d9601c250e71eac975c5d43
Shubham <shubhamsinghmys@gmail.com>
2025-04-16 18:40:54 +0530

    Dialog box added

--- app/src/main/java/com/example/shoppingapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppingapp/ShoppingList.kt
@@ -3,11 +3,15 @@ package com.example.shoppingapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,19 +20,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-data class ShoppingItem(val id: Int,
+data class ShoppingItem(var id: Int,
                         var name: String,
-                        var quality: Int,
+                        var quality: Double,
                         var isEditing: Boolean = false)
-
-
-
 
 @Composable
 fun ShoppingApp(paddingValues: PaddingValues) {
     var shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
+
+    var invalidItemNameDisplay by remember { mutableStateOf(false) }
+    var invalidItemQualityDisplay by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.padding(paddingValues)
@@ -37,7 +45,7 @@ fun ShoppingApp(paddingValues: PaddingValues) {
     ) {
 
         Button(
-            onClick = {},
+            onClick = {showDialog = true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text("Add Item")
@@ -50,5 +58,71 @@ fun ShoppingApp(paddingValues: PaddingValues) {
 
             }
         }
+    }
+
+    if(showDialog){
+        AlertDialog(
+            confirmButton = {
+                Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                        Button(onClick = {
+                            if(itemName.isBlank()){
+                               invalidItemNameDisplay = true
+                            }
+                            if(itemQuantity.toDoubleOrNull() == null)
+                                invalidItemQualityDisplay = true
+
+
+                            if(itemName.isNotBlank() && itemQuantity.toIntOrNull() != null){
+                                val newItem: ShoppingItem = ShoppingItem(
+                                    shoppingItems.size + 1,
+                                    itemName,
+                                    itemQuantity.toDouble()
+                                )
+
+                                itemName = ""
+                                itemQuantity = ""
+                                showDialog = false
+                                invalidItemQualityDisplay = false
+                                invalidItemNameDisplay = false
+                            }
+                        }) { Text("Add")}
+
+                    Button(onClick = {
+                            showDialog = false
+                    }) { Text("Cancel")}
+                }
+            },
+            onDismissRequest = {showDialog = false},
+            title = {Text("Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = {itemName = it},
+                        label = {Text("Item Name")},
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    if(invalidItemNameDisplay)
+                        Text("Item name cannot be blank",
+                            color = Color.Red,
+                            modifier = Modifier.fillMaxWidth().padding(8.dp))
+
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = {itemQuantity = it},
+                        label = {Text("Item Quantity")},
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    if(invalidItemQualityDisplay)
+                        Text("Item name should be a number",
+                            color = Color.Red,
+                            modifier = Modifier.fillMaxWidth().padding(8.dp))
+                }
+            })
     }
 }
