9ba36dca8bc147a6ceec2f4807c282ae633eeb18
Shubham <shubhamsinghmys@gmail.com>
2025-06-16 13:24:58 +0530

    LocationViewModel

---
+++ app/src/main/java/com/example/shoppingapp/LocationViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.shoppingapp
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.State
+
+
+class LocationViewModel: ViewModel() {
+    private val _location = mutableStateOf<LocationData?>(null)
+
+    val location: State<LocationData?> = _location
+
+    fun updateLocation(newLocation: LocationData){
+        _location.value = newLocation
+    }
+}
